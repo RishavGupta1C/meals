9829051801c595ee39f03b260e2a8422141ff312
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2023-09-10 19:55:30 +0530

    commit 9

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
+
+  // Adding a method to a StatelessWidget
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    // Navigator.of(context).push(route);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: ((context) => MealsScreen(
+              title: category.title,
+              meals: filteredMeals,
+            )),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,15 +41,19 @@ class CategoriesScreen extends StatelessWidget {
             childAspectRatio: 3 / 2,
             crossAxisSpacing: 20,
             mainAxisSpacing: 20),
-        children:
-            // alternative for
-            // availableCategories
-            //     .map((category) => CategoryGridItem(category: category))
-            //     .toList(),
-            [
+        children: [
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
+        // alternative for the above for loop
+        // availableCategories
+        //     .map((category) => CategoryGridItem(category: category))
+        //     .toList(),
       ),
     );
   }
