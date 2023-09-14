76f32559f1c3a7ea8d2c97a4918eb71814b662c4
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2023-09-14 23:57:41 +0530

    commit 16

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -5,6 +5,26 @@ class MainDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              colors: [
+                Theme.of(context).colorScheme.primaryContainer,
+                Theme.of(context).colorScheme.primaryContainer.withOpacity(0.8),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )),
+            child: Row(
+              children: [],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
